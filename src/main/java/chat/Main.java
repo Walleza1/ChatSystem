abcebfe2c89cd057c07b62ba93551906c276f0ff
Vincent Erb <vincent.erb@hotmail.fr>
2018-12-12 15:53:19 +0100

    starded working on main window view

--- src/main/java/chat/Main.java
+++ src/main/java/chat/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application implements Observer {
     public int nb_message=0;
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("chat.fxml"));
         primaryStage.setTitle("Clavardage 1.0");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
